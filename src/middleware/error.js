d27e598c5fe2b78e9a3a4a22d047e7fc5a012f81
Your Name <you@example.com>
2025-11-20 23:42:15 +0200

    jgjg

--- src/middleware/error.js
+++
@@ -1,7 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  console.error("❌ Error:", err.message);
-  res.status(500).json({
-    success: false,
-    message: err.message || "Internal Server Error",
-  });
-};
--- src/routes/search.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { searchUsersAndCommunities } from "../controllers/user.controller.js";
-
-const router = express.Router();
-
-router.get("/", searchUsersAndCommunities);
-
-export default router;
