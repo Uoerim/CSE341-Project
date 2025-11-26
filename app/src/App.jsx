eca57f4928f60cef1e5391f9936012f21074d6da
Yosif Ibrahim <65606350+Uoerim@users.noreply.github.com>
2025-11-26 22:05:22 +0200

    Authentication Implemented

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Auth from "./pages/Auth/Auth";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ app/src/components/Auth/AuthNav/AuthNav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./authnav.css";
+
+function AuthNav() {
+  return (
+    <nav>
+      <img src="/logo-white.png" alt="Logo" />
+    </nav>
+  );
+}
+
+export default AuthNav;
