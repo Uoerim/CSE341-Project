b7adfcab6ead22471f5f550e3104feb398109521
N o u r y <nourahossam1211@gmail.com>
2025-12-08 16:33:25 +0200

    Resolve auth conflicts and update routes

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { registerUser, loginUser, checkUsername, checkEmail,} from "../controllers/auth.controller.js";
 import { validate } from "../middleware/validate.js";
+import { registerUser, loginUser, checkUsername, checkEmail, verifyToken, googleAuth, completeGoogleRegistration } from "../controllers/auth.controller.js";
 
 
 const router = express.Router();
@@ -10,5 +11,7 @@ router.post("/login", loginUser);
 router.post("/check-username", checkUsername);
 router.post("/check-email", checkEmail);
 router.get("/verify", verifyToken);
+router.post("/google-auth", googleAuth);
+router.post("/google-register", completeGoogleRegistration);
 
 export default router;
