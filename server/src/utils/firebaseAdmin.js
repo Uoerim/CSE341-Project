f85a3f403d68fa4d2323ab36d6ed4c2066e3567d
Malak Zaiter <malossama.business@gmail.com>
2025-12-12 02:56:27 +0200

    firebase + otp

--- server/src/utils/firebaseAdmin.js
+++ server/src/utils/firebaseAdmin.js
@@ -1,9 +1,15 @@
 import admin from "firebase-admin";
-import serviceAccount from "../config/firebase-service-account.json" assert { type: "json" };
+import dotenv from "dotenv";
+
+dotenv.config();
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
