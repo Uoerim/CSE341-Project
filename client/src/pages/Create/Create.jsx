afae4e9fc1331aaaf833bd90fada713a3aa5774e
Malak Zaiter <malossama.business@gmail.com>
2025-12-19 20:27:30 +0200

    title text box style better

--- client/src/pages/Create/Create.jsx
+++ client/src/pages/Create/Create.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./create.css";
 import RichTextEditor from "../../components/Global/RichTextEditor";
-import Textbox from "../../components/Global/textbox/textbox";
 import CommunitySelectorModal from "../../components/Global/CommunitySelectorModal/CommunitySelectorModal";
 
 function Create({ onNavigateHome }) {
@@ -19,6 +18,14 @@ function Create({ onNavigateHome }) {
     useEffect(() => {
         fetchUserData();
     }, []);
+
+    useEffect(() => {
+        const textarea = document.getElementById('innerTextArea');
+        if (textarea) {
+            textarea.style.height = '20px';
+            textarea.style.height = textarea.scrollHeight + 'px';
+        }
+    }, [title]);
 
     const fetchUserData = async () => {
         try {
@@ -234,12 +241,42 @@ function Create({ onNavigateHome }) {
 
                 <div className="text-editor">
                     <div className="title-input-wrapper">
-                        <Textbox
-                            placeholder="Title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value.substring(0, 300))}
-                            showCheckmark={true}
-                        />
+                        <div className="label-container interior-label">
+                            <span className="input-boundary-box">
+                                <span className={`input-container ${title ? 'activated' : ''}`}>
+                                    <span className="label-text inner-label" id="fp-input-label">
+                                        <span>Title</span>
+                                        <span className="required-asterisk" aria-hidden="true">*</span>
+                                    </span>
+                                    <div className="text-area-wrapper" style={{ '--textarea-minheight': '20px' }}>
+                                        <textarea
+                                            aria-labelledby="fp-input-label"
+                                            id="innerTextArea"
+                                            className=""
+                                            name="title"
+                                            maxLength="300"
+                                            required
+                                            value={title}
+                                            onChange={(e) => setTitle(e.target.value.substring(0, 300))}
+                                            style={{ height: '20px' }}
+                                        />
+                                    </div>
+                                </span>
+                                <span id="trailing-icons-container">
+                                    <span id="trailing-icons-validation">
+                                        {title && title.length === 0 ? (
+                                            <svg rpl="" className="trailing-icon invalid" fill="currentColor" height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M11.21 13.5a1.21 1.21 0 11-2.42 0 1.21 1.21 0 012.42 0zM19 10c0-4.963-4.038-9-9-9s-9 4.037-9 9 4.038 9 9 9 9-4.037 9-9zm-1.801 0c0 3.97-3.229 7.2-7.199 7.2-3.97 0-7.199-3.23-7.199-7.2S6.03 2.8 10 2.8c3.97 0 7.199 3.23 7.199 7.2zm-6.441 1.24l.242-6H9l.242 6h1.516z"></path>
+                                            </svg>
+                                        ) : title && title.length > 0 ? (
+                                            <svg rpl="" className="trailing-icon valid" fill="currentColor" height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M7.09 15.84c-.23 0-.46-.09-.64-.26L1.3 10.42a.9.9 0 010-1.27.9.9 0 011.27 0l4.52 4.52L17.5 3.26a.9.9 0 011.27 0 .9.9 0 010 1.27L7.73 15.58c-.18.18-.41.26-.64.26z"></path>
+                                            </svg>
+                                        ) : null}
+                                    </span>
+                                </span>
+                            </span>
+                        </div>
                         <div className="title-counter">{title.length}/300</div>
                     </div>
 
