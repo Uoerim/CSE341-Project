2383d5c85ae5f8bb5cfb2b44e7f52d2973e7e39d
Your Name <you@example.com>
2025-12-20 00:16:21 +0200

    backend error handled

--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -627,6 +627,8 @@ export const getExplorePosts = async (req, res) => {
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
 // SAVE post (toggle)
 export const savePost = async (req, res, next) => {
   try {
